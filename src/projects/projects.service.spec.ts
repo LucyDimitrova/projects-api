1565ada98f97e4140c58df5600ec39fd7e7f3c82
LucyDimitrova <lucy@riseart.com>
2024-01-22 19:59:14 +0200

    inject repository for testing

--- src/projects/projects.service.spec.ts
+++ src/projects/projects.service.spec.ts
@@ -1,18 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProjectsService } from './projects.service';
+import { Repository } from "typeorm";
+import { Project } from "./entities/project.entity";
+import { getRepositoryToken } from "@nestjs/typeorm";
 
 describe('ProjectsService', () => {
-  let service: ProjectsService;
+  let projectsService: ProjectsService;
+  let projectsRepository: Repository<Project>;
+
+  const projectsRepositoryToken: string | Function =
+    getRepositoryToken(Project);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProjectsService],
+      providers: [
+        ProjectsService,
+        {
+          provide: projectsRepositoryToken,
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
-    service = module.get<ProjectsService>(ProjectsService);
+    projectsService = module.get<ProjectsService>(ProjectsService);
+    projectsRepository = module.get<Repository<Project>>(
+      projectsRepositoryToken,
+    );
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(projectsService).toBeDefined();
   });
 });
